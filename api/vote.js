60f84569289cb35727d3eba3c2047027fa0f938d
philippe-source <philippe@yourmood.net>
2026-03-18 12:53:10 +0100

    Update vote.js

--- api/vote.js
+++ api/vote.js
@@ -1,6 +1,16 @@
 import crypto from 'crypto';
 import { createClient } from '@supabase/supabase-js';
 
+module.exports = async function handler(req, res) {
+
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
 const supabase = createClient(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_SERVICE_ROLE_KEY
