3d5b619eb5be3059520282adb49eebea81ecacc8
philippe-source <philippe@yourmood.net>
2026-03-18 12:53:44 +0100

    Update votes.js

--- api/votes.js
+++ api/votes.js
@@ -1,5 +1,15 @@
 import { createClient } from '@supabase/supabase-js';
 
+module.exports = async function handler(req, res) {
+
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
 const supabase = createClient(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_SERVICE_ROLE_KEY
